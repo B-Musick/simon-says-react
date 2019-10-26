8a0d67e002aea6d7e1d68faf2de2d8994a8d091e
B-Musick <bmuze1@gmail.com>
2019-10-26 16:12:42 -0600

    Added startGame() method which will set playGame state value to true when press Play Game button. This will then call addColorToPattern() when updated

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,23 +5,37 @@ class Board extends React.Component {
     state = {
         buttons: ['red', 'blue', 'green', 'yellow'], // Holds the color of the buttons
         pattern: [], // This will hold the pattern the user clicks
-        patternToMatch: [] // holds the pattern that should flash
+        patternToMatch: [], // holds the pattern that should flash
+        playGame: false // Used for when user presses button to play game
     };
 
+    componentDidUpdate(prevProps,prevState){
+        
+        if (!prevState.playGame && this.state.playGame){
+            this.addColorToPattern();
+        }
+    }
     addColorToPattern=()=>{
         // This will add a new pattern to the pattern array
 
         // Pick random value between 0 and 3 which will be index of color chosen
         let randomVal = Math.floor(Math.random()*4); // Choose value between 0 and 3
-        let randomColor = this.props.buttons[randomVal];
+        let randomColor = this.state.buttons[randomVal];
+        console.log(randomColor)
         this.setState(prevState=>({
-            patternToMatch: [...prevState,randomColor]
+            patternToMatch: [...prevState.patternToMatch,randomColor]
         }));
     }
-    compareClicks(){
+    compareClicks=()=>{
         // This will compare the clicks the person made and the pattern
         // If pattern was successful, then call addColorToPattern
-        // return this.state.patternToMatch.every(val=>{val===this.state.pattern});
+        console.log(this.state.patternToMatch.every((val, index) => {return val === this.state.pattern[index] }))
+        // return this.state.patternToMatch.every((val, index) => { val === this.state.pattern[index] });
+    }
+    startGame=()=>{
+        this.setState({
+            playGame:true
+        })
     }
     handleClick=(val)=>{
         // This will place the click pattern into an array
@@ -31,20 +45,22 @@ class Board extends React.Component {
         this.setState(prevState=>({
             pattern: [...prevState.pattern,val]
         }));
-        // setTimeout(()=>{
-        //     // Give them 2s to press the nextbutton
-        //     this.compareClicks();
-        // },2000)
+        setTimeout(()=>{
+            // Give them 2s to press the nextbutton
+            this.compareClicks();
+        },2000)
     }
 
     render() {
-        console.log(this.state.pattern);
+        console.log(this.state.patternToMatch);
+        console.log(this.state.playGame)
         return (
             <div>
                 <Button color={this.state.buttons[0]} handleClick={this.handleClick}/>
                 <Button color={this.state.buttons[1]} handleClick={this.handleClick} />
                 <Button color={this.state.buttons[2]} handleClick={this.handleClick} />
                 <Button color={this.state.buttons[3]} handleClick={this.handleClick} />
+                <button onClick={this.startGame}>Play Game</button>
             </div>
         )
     }
